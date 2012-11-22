4240a41fc1b1bafe016c1b7fd41a596c173cd5a7
Andrew Mauboussin <ammaub@college.harvard.edu>
2012-11-22 18:07:11 -0500

    created basic version of site

---
+++ q/viewer/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from viewer.models import Qcourses
+
+def index(request):
+    classes = Qcourses.objects.order_by('-enrollment')[:20]
+    return render_to_response('index.html', {'classes': classes})
