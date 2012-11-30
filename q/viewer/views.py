906090312e8a792a4d43353af175845d6cefc78e
Andrew Mauboussin <ammaub@college.harvard.edu>
2012-11-30 17:30:49 -0500

    added in bootstrap, set up static files and less

--- q/viewer/views.py
+++ q/viewer/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from viewer.models import Qcourses
+from django.template.context import RequestContext
 
 def index(request):
     classes = Qcourses.objects.order_by('-enrollment')[:20]
-    return render_to_response('index.html', {'classes': classes})
+    return render_to_response('index.html', {'classes': classes}, context_instance=RequestContext(request))
