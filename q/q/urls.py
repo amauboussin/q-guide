c210b317c1c7c852ef5851ed09afeb69a0cfaaea
Josh Palay <josh@localhost.localdomain>
2013-01-04 12:08:23 -0600

    Adds support for viewing courses in specified term/year

--- q/q/urls.py
+++ q/q/urls.py
@@ -13,7 +13,10 @@ urlpatterns = patterns('',
     url(r'^courses/search/$', course_search_results, name='search'), #course search
     url(r'^courses/top/$', top_courses, name='top_courses'), # top courses page. parameters are defined by get variables
     url(r'^courses/([a-zA-Z&-]+)/$', department_view, name='department'), #view all of the courses in the department
-    url(r'^courses/(?P<course_field>[a-zA-Z&-]+)/(?P<course_number>[0-9.a-zA-Z]+)/$', course_detail, name = "course_detail" ), #detailed view of a course
+    url(r'^courses/(?P<course_field>[a-zA-Z&-]+)/(?P<course_number>[0-9.a-zA-Z]+)/(?P<year>[0-9]+)/(?P<term>[a-zA-Z]+)/$', course_detail, name = "course_detail" ), #detailed view of a course with defined year/term
+    url(r'^courses/(?P<course_field>[a-zA-Z&-]+)/(?P<course_number>[0-9.a-zA-Z]+)/(?P<year>[0-9]+)/$', course_detail, name = "course_detail" ), #detailed view of a course with defined year
+    url(r'^courses/(?P<course_field>[a-zA-Z&-]+)/(?P<course_number>[0-9.a-zA-Z]+)/(?P<term>[a-zA-Z]+)/$', course_detail, name = "course_detail" ), #detailed view of a course with defined term, defaults to most current year
+    url(r'^courses/(?P<course_field>[a-zA-Z&-]+)/(?P<course_number>[0-9.a-zA-Z]+)/$', course_detail, name = "course_detail" ), #detailed view of a course, defaults to most current year/term
     url(r'^profs/search/$', prof_search_results, name = "prof_search" ), #page with professor search box
     url(r'^profs/(?P<id>\w+)/$', prof_detail, name = "prof_detail" ), #all of the professor's courses in one page
 
--- q/viewer/views.py
+++ q/viewer/views.py
@@ -181,11 +181,20 @@ def prof_search_results(request):
         context_instance=RequestContext(request))
 
 #detailed view of a single course 
-def course_detail(request, course_field, course_number):
+def course_detail(request, course_field, course_number, year = None, term = None):
+    
     course_number = string.replace(string.upper(course_number),'_',' ')
-
     #get all instances of the course
-    courses = Qcourses.objects.filter(field__exact = string.upper(course_field)).filter(number__exact = course_number).order_by('-year')
+    courses = Qcourses.objects.filter(field__exact = string.upper(course_field)).filter(number__exact = string.upper(course_number)).order_by('-term').order_by('-year')
+    
+    if year is not None:
+        courses = courses.filter(year__exact = year).order_by('-term')
+    if term is not None:
+        if string.upper(term) != "FALL" and string.upper(term) != "SPRING":
+            return render_to_response('no_course_found.html', {}, context_instance=RequestContext(request))
+
+        term_num = 1 if string.upper(term) == "FALL" else 2
+        courses = courses.filter(term__exact = term_num).order_by('-year')
 
     if not courses:
         return render_to_response('no_course_found.html', {}, context_instance=RequestContext(request))
@@ -193,6 +202,7 @@ def course_detail(request, course_field, course_number):
     #add a comments attribute to each course with all of its comments
     for course in courses:
         course.comments = Qcomments.objects.filter(course_id = course.course_id)
+
 
     return render_to_response('course.html', {'courses': courses}, context_instance=RequestContext(request))
 
