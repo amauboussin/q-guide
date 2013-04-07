c74fb28a5bb47068f5d9acab3043027d2fe33294
Josh Palay <joshpalay@gmail.com>
2013-04-07 19:25:47 -0400

    changed STATIC_DIR

--- q/q/settings.py
+++ q/q/settings.py
@@ -57,7 +57,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/q2/q-guide/q/viewer/static/'
+STATIC_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
