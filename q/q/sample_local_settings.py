7c2e25fa4c0561c0ba72191c8af6c542d7e7a03a
Josh Palay <joshpalay@gmail.com>
2013-04-07 16:20:54 -0400

    cleaned up .gitignore, added local_settings.py, moved templates

---
+++ q/q/sample_local_settings.py
@@ -0,0 +1,17 @@
+import os
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'qdata',        # Or path to database file if using sqlite3.
+        'USER': 'qdata',        # Not used with sqlite3.
+        'PASSWORD': 'password', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+STATIC_ROOT = (os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static'))
+TEMPLATE_DIRS = (os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates'))
